ab383ffd8be1c24d7d1c00d5310d30a5afc2b69e
lsh02002 <lsh02002@hanmail.net>
2026-08-22 10:20:32 +0900

    add router system

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import App from "./App";
+import App from "./components/layout/LayoutEditor";
 import { LoginProvider } from "./context/LoginContext";
 import { ModalProvider } from "./context/ModalContext";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <LoginProvider>
-    <ModalProvider>      
-      <App />
+    <ModalProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ModalProvider>
   </LoginProvider>,
 );
