3a15044c56df7b8b5ad4f3289b67693473f8b466
lsh02002 <lsh02002@hanmail.net>
2026-09-07 17:16:31 +0900

    Update EditBasicTab.tsx

--- src/components/editor/edit/tabs/EditBasicTab.tsx
+++ src/components/editor/edit/tabs/EditBasicTab.tsx
@@ -1,9 +1,6 @@
 import type { LayoutComponent } from "../../../../types/types";
 import type { ComponentRegistry } from "../../registry/componentRegistry";
-import {
-  getComponentDefinition,
-  renderComponentEditor,
-} from "../../registry/componentRegistry";
+import { renderComponentEditor } from "../../registry/componentRegistry";
 
 type Props = {
   componentRegistry: ComponentRegistry;
@@ -19,29 +16,6 @@ function EditBasicTab({
   component,
   updateComponent,
 }: Props) {
-  const definition = getComponentDefinition(componentRegistry, component.type);
-  const supportsDisabled = definition.supportsDisabled === true;
-
-  const disabled =
-    "disabled" in component.props && component.props.disabled === true;
-
-  const handleDisabledChange = (value: boolean) => {
-    updateComponent((current) => {
-      if (!("disabled" in current.props)) {
-        return current;
-      }
-
-      return {
-        ...current,
-
-        props: {
-          ...current.props,
-          disabled: value,
-        },
-      } as LayoutComponent;
-    });
-  };
-
   return (
     <>
       <div className="mb-3">
@@ -56,24 +30,6 @@ function EditBasicTab({
       </div>
 
       {renderComponentEditor(componentRegistry, { component, updateComponent })}
-
-      {supportsDisabled && (
-        <div className="form-check mb-3 mt-3">
-          <input
-            id="editDisabled"
-            type="checkbox"
-            className="form-check-input"
-            checked={disabled}
-            onChange={(event) => {
-              handleDisabledChange(event.target.checked);
-            }}
-          />
-
-          <label className="form-check-label" htmlFor="editDisabled">
-            Disabled
-          </label>
-        </div>
-      )}
     </>
   );
 }
